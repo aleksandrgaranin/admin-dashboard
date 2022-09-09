26c48799f256fc85366a8eb3ca930747212f37a2
alek <aleksandrgaranin89@gmail.com>
2022-09-09 14:14:34 -0700

    Bug fixed and Line Chart added

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,36 @@
+import { fromJSON } from 'postcss'
 import React from 'react'
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective, Inject,
+  DateTime, Legend, Tooltip, LineSeries
+} from '@syncfusion/ej2-react-charts'
 
-function LineChart() {
+import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy'
+
+import { useStateContext } from '../../context/contextProvider'
+
+const LineChart = () => {
+  const { currentMode } = useStateContext()
+
   return (
-    <div>LineChart</div>
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({ currentColor, id, type, height,
-  width, data, color }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName="x"
-      yName="yval"
-      type={type}
-      opacity={1}
-      tooltipSettings={{
-        visible: true, 
-        format: '${x} : data ${yval}',
-        trackLineSettings: {
-          visible: true
-        }
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  )
+class SparkLine extends React.PureComponent {
+  render() {
+    const { currentColor, id, type, height,
+      width, data, color } = this.props
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+        opacity={1}
+        tooltipSettings={{
+          visible: true,
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true
+          }
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    )
+  }
 }
+
+
 
 export default SparkLine
